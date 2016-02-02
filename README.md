7bd20b3cb679278bc5a9654df80c40b4c1ac079e
Edward Nikolaev <edward.nikolaev@mitridat.kiev.ua>
2016-02-02 16:52:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+JWT Single Sign-On examples for Helprace
+========================================
+Documentation
+-------------
+All documentation on how to implement Single Sign-On with Helprace can be found here:[Setting up Single Sign-On based on JWT (JSON Web Tocken)](http://support.helprace.com/i4-setting-up-single-sign-on-based-on-jwt-json-web-tocken).
+
+Contributing
+------------
+New examples and pull requests are welcome.
---
+++ php_jwt.php
@@ -0,0 +1,36 @@
+<?php
+ini_set('display_startup_errors', '1');
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+// Be sure to upload Firebase JWT library from https://github.com/firebase/php-jwt to "jwt" subdirectory
+require __DIR__ . "/jwt/JWT.php";
+require __DIR__ . "/jwt/BeforeValidException.php";
+require __DIR__ . "/jwt/ExpiredException.php";
+require __DIR__ . "/jwt/SignatureInvalidException.php";
+use Firebase\JWT\JWT;
+
+// Log the user in
+
+$key       = ""; 	// SSO Key from Settings > Authentication
+$subdomain = ""; 	// Your Helprace account subdomain, e.g. "companyname"
+$now       = time();
+
+$token = array(
+  "jti"                 =>  uniqid('', true),
+  "iat"                 =>  $now,
+  "email"               =>  "user@example.com",
+  "name"                =>  "Odiban Canopi",        	// Full name
+  "organization"        =>  "Mycompany",       		// Organization name
+  "organization_url"    =>  "http://example.com",	// Link to the organization website (starting with http or https)
+  "job_title"           =>  "My Job Title",		// Job title
+  "avatar"              =>  "",				// URL to user's avatar (starting with http or https)
+  "role"                =>  "user",                  	// Role could be: user, agent, admin, owner
+  "external_id"         =>  "1"                   	// External ID can be used to uniquely identify user. If not specified, email is used as a unique user ID.
+);
+
+$jwt = JWT::encode($token, $key);
+$location = "https://auth.helprace.com/jwt/" . $subdomain ."?jwt=" . $jwt;
+
+// Redirect
+header("Location: " . $location);
